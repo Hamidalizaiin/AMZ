98f0de681c22021434acfe017e09a54f3e0e0eed
Hamidalizaiin <hamidalizain13@gmail.com>
2025-10-21 22:42:55 +0500

    New Contact  page added to the route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Dashboard from './pages/Dashboard';
+import About from './pages/About'; 
 import { Home } from './pages/Home/Home';
 import Footer from './components/Footer';
+import Contact from './pages/Contact Us/Contact';
 
 const App: React.FC = () => {
   return (
@@ -16,8 +15,7 @@ const App: React.FC = () => {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
+         </Routes>
         <Footer />
       </div>
     </Router>
